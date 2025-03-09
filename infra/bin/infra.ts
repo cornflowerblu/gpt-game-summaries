1c0e00c4d52d54948250ae114cdf0836bd96629f
Roger Urich <rogerdurich+git@gmail.com>
2025-03-09 04:21:18 -0500

    noice

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,6 +3,7 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { OvertimeOtePlayerSummariesStack } from '../lib/overtime-ote-player-summaries-stack';
 import { MongoDBStack } from '../lib/mongodb-stack';
+import { OvertimePipelineStack } from '../lib/pipeline-stack';
 
 const app = new cdk.App();
 
@@ -26,3 +27,17 @@ const appStack = new OvertimeOtePlayerSummariesStack(app, 'OvertimeOtePlayerSumm
 
 // Add dependency to ensure MongoDB is deployed first
 appStack.addDependency(mongoDbStack);
+
+new OvertimePipelineStack(app, 'OvertimePipelineStack', {
+  ecrRepository: appStack.ecrRepository, // Fix reference to use infraStack
+  ecsCluster: 'overtime-cluster',
+  ecsService: 'overtime-ote-player-summaries-80', 
+  githubOwner: 'cornflowerblu',
+  githubRepo: 'gpt-game-summaries',
+  githubBranch: 'amazon-q-developer',
+  codestarConnectionArn: 'arn:aws:codeconnections:us-east-1:443370689229:connection/91220065-4063-449c-a1e1-682054889c6e',
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION
+  }
+});
